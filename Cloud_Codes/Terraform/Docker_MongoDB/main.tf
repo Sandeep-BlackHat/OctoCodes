db58263d8e82e0291792e8fcf10c6a09bc39efaf
Rajavamshi-Daath <110937169+Rajavamshi-Daath@users.noreply.github.com>
2022-10-01 22:46:34 -0400

    Update main.tf

--- Cloud_Codes/Terraform/Docker_MongoDB/main.tf
+++ Cloud_Codes/Terraform/Docker_MongoDB/main.tf
@@ -4,17 +4,12 @@ terraform {
     username = "Admin"
     password = "password"
     lock_address = "http://localhost:8000/states/omni_tools/lock/"
-    unlock_address = "http://localhost:8000/states/omni_tools/unlock/"
-  }
+    unlock_address = "http://localhost:8000/states/omni_tools/unlock/"}
   required_providers {
     local = {
       source = "hashicorp/local"
-      version = "2.0.0"
-    }
-  }
-}
+      version = "2.0.0"}}}
 provider "local" {}
 resource "local_file" "hello" {
   content = "Hello Sudakar's team"
-  filename = "omni_tools.txt"
-}
+  filename = "omni_tools.txt"}
